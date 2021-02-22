77e0dde33c7a545fd3e1d411151c5562f7a1a3d3
sjschweber <schweber97@gmail.com>
2021-02-22 13:46:41 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import {data} from './data.js';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="header">
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    name: "Drago",
+    personality: "Lazy",
+    catchphrase: "burrn",
+    ranking: "B",
+    type: "Aligator"
+  },
+  {
+    name: "Del",
+    personality: "Cranky",
+    catchphrase: "gronk",
+    ranking: "C",
+    type: "Aligator"
+  },
+  {
+    name: "Gayle",
+    personality: "Normal",
+    catchphrase: "snacky",
+    ranking: "C",
+    type: "Aligator"
+  }
+]
